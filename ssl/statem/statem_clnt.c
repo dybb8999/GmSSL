6800b7faef2ccb0c86a5c132ab058600c130afc2
Hell <wzz8999@163.com>
2021-03-16 17:13:57 +0800

    修改密钥选择
    不再使用最后一次调用SSL_CTX_use_PrivateKey_file 所设置的密钥。而是进行判断

--- ssl/statem/statem_clnt.c
+++ ssl/statem/statem_clnt.c
@@ -2772,6 +2772,16 @@ int tls_construct_client_verify(SSL *s)
     }
 
     p = ssl_handshake_start(s);
+#ifndef OPENSSL_NO_GMTLS
+    if (SSL_IS_GMTLS(s))
+    {
+        pkey = s->cert->pkeys[SSL_PKEY_SM2].privatekey;
+    }
+    else
+    {
+        pkey = s->cert->key->privatekey;
+    }
+#endif
     pkey = s->cert->key->privatekey;
 
     hdatalen = BIO_get_mem_data(s->s3->handshake_buffer, &hdata);
